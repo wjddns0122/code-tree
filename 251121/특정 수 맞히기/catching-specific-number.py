4cea0e3f2a30552640a87eab35481b4b89767cfe
JungWoon <64579991+wjddns0122@users.noreply.github.com>
2025-11-21 16:59:24 +0900

    Title: 특정 수 맞히기, Time: 82ms, Memory: 19MB, Status: Runtime Error - Codetree

---
+++ 251121/특정 수 맞히기/catching-specific-number.py
@@ -0,0 +1,11 @@
+while True:
+    n = int(input())
+    if n > 25:
+        print('Lower')
+        continue
+    elif n < 25:
+        print('Higher')
+        continue
+    elif n == 25:
+        print("Good")
+        breakwh
