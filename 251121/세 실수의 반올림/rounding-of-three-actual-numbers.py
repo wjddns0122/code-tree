639cfb877b19eafefc49d865eb478c1851682937
JungWoon <64579991+wjddns0122@users.noreply.github.com>
2025-11-21 22:13:33 +0900

    Title: 세 실수의 반올림, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 251121/세 실수의 반올림/rounding-of-three-actual-numbers.py
+++ 251121/세 실수의 반올림/rounding-of-three-actual-numbers.py
@@ -2,10 +2,6 @@ a = float(input())
 b = float(input())
 c = float(input())
 
-result1 = round(a, 3)
-result2 = round(b, 3)
-result3 = round(c, 3)
-
-print(f"{result1:.3f}")
-print(f"{result2:.3f}")
-print(f"{result3:.3f}")
+print(f"{a:.3f}")
+print(f"{b:.3f}")
+print(f"{c:.3f}")
