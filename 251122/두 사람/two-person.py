026b5b0f70b77d9ff03aeae58eea3747a5a34af4
JungWoon <64579991+wjddns0122@users.noreply.github.com>
2025-11-22 02:37:27 +0900

    Title: 두 사람, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 251122/두 사람/two-person.py
+++ 251122/두 사람/two-person.py
@@ -3,9 +3,11 @@ a2, s2 = input().split()
 a1 = int(a1)
 a2 = int(a2)
 
-if (a1 >= 19 and a2 >= 19) and (s1 == "M" or s2 == "M"):
+if (a1 >= 19 and s1 == "M") and (a2 >= 19 and s2 == "M"):
     print(1)
-elif (a1 >= 19 or a2 >= 19) and (s1 == "M" or s2 == "M"):
+elif a1 >= 19 and s1 == "M":
+    print(1)
+elif a2 >= 19 and s2 == "M":
     print(1)
 else:
     print(0)
