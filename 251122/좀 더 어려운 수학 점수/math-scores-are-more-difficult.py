ee5ee29127c37cdd7daac0b23c23cd8d74751300
JungWoon <64579991+wjddns0122@users.noreply.github.com>
2025-11-22 02:06:01 +0900

    Title: 좀 더 어려운 수학 점수, Time: 48ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251122/좀 더 어려운 수학 점수/math-scores-are-more-difficult.py
@@ -0,0 +1,14 @@
+math_a, eng_a = list(map(int, input().split()))
+math_b, eng_b = list(map(int, input().split()))
+
+if math_a > math_b:
+    print('A')
+
+if math_a == math_b:
+    if eng_a > eng_b:
+        print('A')
+    else:
+        print('B')
+    
+else:
+    print('B')
