40f151f98711a8518a5b72b2939abbb84355979c
JungWoon <64579991+wjddns0122@users.noreply.github.com>
2025-11-22 01:18:29 +0900

    Title: 세 정수의 최솟값, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251122/세 정수의 최솟값/minimum-of-three-numbers.py
+++ 251122/세 정수의 최솟값/minimum-of-three-numbers.py
@@ -2,7 +2,7 @@ a, b, c = list(map(int, input().split()))
 
 if a <= b <= c and a <= c <= b:
     print(a)
-elif b <= a <= c and b <= c <= a:
+if b <= a <= c and b <= c <= a:
     print(b)
-elif c <= b <= a and c <= a <= b:
+if c <= b <= a and c <= a <= b:
     print(c)
