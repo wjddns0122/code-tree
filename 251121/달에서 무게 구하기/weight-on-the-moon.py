cf4ba7cdcadb4fd27d90d2fa9cc20e04cc860c27
JungWoon <64579991+wjddns0122@users.noreply.github.com>
2025-11-21 16:44:58 +0900

    Title: 달에서 무게 구하기, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251121/달에서 무게 구하기/weight-on-the-moon.py
@@ -0,0 +1,7 @@
+weight = 13
+gravity = 0.165000
+
+result = weight * gravity
+gravity = '%0.6f' % 0.165000
+result1 = '%0.6f' % result
+print(str(weight) + " * " + str(gravity) + " = " + str(result1))
