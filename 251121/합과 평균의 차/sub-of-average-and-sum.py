e12c7dbb854ae173bde254d4b1d08dc2a54ea92f
JungWoon <64579991+wjddns0122@users.noreply.github.com>
2025-11-21 23:19:09 +0900

    Title: 합과 평균의 차, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251121/합과 평균의 차/sub-of-average-and-sum.py
+++ 251121/합과 평균의 차/sub-of-average-and-sum.py
@@ -1,8 +1,5 @@
 a, b, c = list(map(int, input().split()))
-
-add = a + b + c
-avg = add / 3
-result = add - avg
-print(int(add))
-print(int(avg))
-print(int(result))
+result = a + b + c
+print(result)
+print(result / 3)
+print(result - (result / 3))
