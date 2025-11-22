1cf1f954c376b7b777d96709a1b41c2778338605
JungWoon <64579991+wjddns0122@users.noreply.github.com>
2025-11-22 18:27:30 +0900

    Title: a의 배수, Time: 47ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251122/a의 배수/multiple-of-a.py
+++ 251122/a의 배수/multiple-of-a.py
@@ -1,13 +1,9 @@
 n, a = list(map(int, input().split()))
 
-if n % a == 0:
-    print(1)
-else:
-    print(0)
 
-while n > 1:
-    n -= 1    
+while n >= 1:
     if n % a == 0:
         print(1)
     else:
         print(0)
+    n -= 1    
