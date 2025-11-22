3920bcc26276ed360534ce175c13de162b740db3
JungWoon <64579991+wjddns0122@users.noreply.github.com>
2025-11-22 19:18:25 +0900

    Title: 369 게임, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 251122/369 게임/369-game.py
+++ 251122/369 게임/369-game.py
@@ -6,7 +6,7 @@ for i in range(1, n + 1):
         print(0, end=" ")
     elif i % 10 == 3 or i % 10 == 6 or i % 10 == 9:
         print(0, end=" ")
-    elif i // 30 == 1 or i // 60 == 1 or i // 90 == 1:
+    elif i // 10 == 3 or i // 10 == 6 or i // 10 == 9:
         print(0, end=" ")
     else:
         print(i, end=" ")
