a1508fdc9208254d16cc5fb141bff2059a6db564
JungWoon <64579991+wjddns0122@users.noreply.github.com>
2025-11-23 18:29:23 +0900

    Title: 1까지 나누기, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 251123/1까지 나누기/divide-by-1.py
+++ 251123/1까지 나누기/divide-by-1.py
@@ -1,12 +1,10 @@
 n = int(input())
 cnt = 0
-result = 0 
-for i in range(1, n + 1):
-    if n % i == 0:
-        result = int(n / i)
-        cnt += 1
-        if result == 0:
-            break
 
-cnt -= 1
+for i in range(1, n + 1):
+    n = int(n / i)
+    cnt += 1
+    if n <= 1:
+        break
+    
 print(cnt)
