396438b2ba51bd3ae25b7844cba76cc5cc60cf19
JungWoon <64579991+wjddns0122@users.noreply.github.com>
2025-11-23 19:12:13 +0900

    Title: 3N + 1 수열, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251123/3N + 1 수열/3n-plus-1-sequence.py
@@ -0,0 +1,14 @@
+n = int(input())
+cnt = 0
+
+while True:
+    if n == 1:
+        break
+    elif n % 2 == 0:
+        n = n // 2
+        cnt += 1
+    elif n % 2 == 1:
+        n = n * 3 + 1
+        cnt += 1
+
+print(cnt)
