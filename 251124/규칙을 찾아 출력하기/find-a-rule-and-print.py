fda5c36a0da790c209169ca1a7489f2a62b67a8f
JungWoon <64579991+wjddns0122@users.noreply.github.com>
2025-11-24 21:48:04 +0900

    Title: 규칙을 찾아 출력하기, Time: 49ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251124/규칙을 찾아 출력하기/find-a-rule-and-print.py
@@ -0,0 +1,11 @@
+n=int(input())
+for i in range(n):
+    for j in range(n):
+        if i==0 or j==n-1:
+            print("*",end=" ")
+        else:
+            if i > j:
+                print("*",end=" ")
+            else:
+                print(" ",end=" ")
+    print()
