47624a8be0aa7d428342eb0a11004bf8ef68d09b
JungWoon <64579991+wjddns0122@users.noreply.github.com>
2025-11-24 10:41:37 +0900

    Title: 별표 출력하기 3, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 251124/별표 출력하기 3/print-star-3.py
+++ 251124/별표 출력하기 3/print-star-3.py
@@ -1,9 +1,10 @@
+# 변수 선언 및 입력
 n = int(input())
 
-cnt = n * 2 -1
-emp = 0
-for _ in range(n):
-    print(' ' * emp, end = '')
-    print('* ' * cnt)
-    cnt -= 2
-    emp += 2
+# 모양에 맞게 별을 출력합니다.
+for i in range(n):
+	for _ in range(i):
+		print(" ", end=" ")
+	for _ in range((2 * n) - (2 * i) - 1):
+		print("*", end=" ")
+	print()
