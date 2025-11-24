63faa7033cb4accdc3a99af54f3c724caa709c2e
JungWoon <64579991+wjddns0122@users.noreply.github.com>
2025-11-24 12:34:06 +0900

    Title: 별표 출력하기 10, Time: 49ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251124/별표 출력하기 10/print-star-10.py
+++ 251124/별표 출력하기 10/print-star-10.py
@@ -3,23 +3,25 @@ n = int(input())
 rows = 2 * n  # 전체 줄 수
 
 for i in range(1, rows + 1):
-    # 대칭 위치를 이용해서 윗부분 인덱스 j 계산
+    # 위쪽/아래쪽 대칭을 위해 기준 줄 번호 t 만들기 (1 ~ n)
     if i <= n:
-        j = i
+        t = i
     else:
-        j = 2 * n + 1 - i   # 위쪽에서 대응되는 줄 번호
+        t = 2 * n + 1 - i  # 위쪽에서 대칭되는 줄 번호
 
-    # 윗부분 규칙에 따라 별 개수 정하기
-    if j == 1:
+    # t번째 줄에서 찍을 별 개수(cnt) 결정
+    if t == 1:
         cnt = 1
-    elif j == 2:
+    elif t == 2:
         cnt = n
-    else:
-        cnt = j - 1
+    elif t == 3:
+        cnt = 2
+    else:  # t >= 4
+        cnt = n - t + 3   # N-1, N-2, ..., 3 이런 식으로 내려감
 
-    # 별 찍기 (* 사이에 공백 넣기)
+    # 별 cnt개를 " *" 사이에 공백 넣어서 출력
     for k in range(cnt):
         print("*", end="")
-        if k != cnt - 1:    # 마지막 별 뒤에는 공백 안 넣기
+        if k != cnt - 1:   # 마지막 별 뒤에는 공백 X
             print(" ", end="")
     print()
