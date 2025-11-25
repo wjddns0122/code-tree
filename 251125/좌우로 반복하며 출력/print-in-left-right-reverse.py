39986b55933a6cebe8aaa43952e0a747802fb4e0
JungWoon <64579991+wjddns0122@users.noreply.github.com>
2025-11-25 22:18:15 +0900

    Title: 좌우로 반복하며 출력, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

--- 251125/좌우로 반복하며 출력/print-in-left-right-reverse.py
+++ 251125/좌우로 반복하며 출력/print-in-left-right-reverse.py
@@ -1,11 +1,20 @@
 n = int(input())
-cnt = 10
+cnt = 1
 
 for i in range(n):
+    row = []
+
     for j in range(n):
-        cnt -= 1
-        if cnt == 0:
-            cnt = 10
-            cnt -= 1
-        print(cnt,end='')
+        row.append(cnt)
+        cnt += 1
+        if (cnt == n + 1):
+            cnt = 1
+
+    if i % 2 != 0:
+        for j in range(n - 1, -1, -1):
+            print(row[j], end="")
+    else:
+        for j in range(n):
+            print(row[j], end="")
+
     print()
