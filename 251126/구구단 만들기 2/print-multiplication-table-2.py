2e5887cbcf5fb4dc0c108dd293f848358dadf1f5
JungWoon <64579991+wjddns0122@users.noreply.github.com>
2025-11-26 14:57:12 +0900

    Title: 구구단 만들기 2, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251126/구구단 만들기 2/print-multiplication-table-2.py
@@ -0,0 +1,8 @@
+a, b = list(map(int, input().split()))
+
+for i in range(2, 10, 2):
+    for j in range(b, a - 1, -1):
+        print(f"{j} * {i} = {i * j}", end=" ")
+        if j > a:
+            print("/", end=" ")
+    print()
