bf23ccdc3504f1c2ceb88c1af9bf1d587143698c
JungWoon <64579991+wjddns0122@users.noreply.github.com>
2025-11-27 22:17:39 +0900

    Title: 1-9 개수 세기, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 251127/1-9 개수 세기/count-one-to-nine.py
+++ 251127/1-9 개수 세기/count-one-to-nine.py
@@ -1,1 +1,10 @@
 n = int(input())
+arr = list(map(int, input().split()))
+count_arr = [0] * 100
+
+for elem in arr:
+    count_arr[elem] += 1
+
+for i in range(1, 10):
+    cnt = count_arr[i]
+    print(cnt)
