f22850528b8407a8854957e60aecb9e7926f1db9
JungWoon <64579991+wjddns0122@users.noreply.github.com>
2025-11-27 22:04:18 +0900

    Title: 100 도달하기, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 251127/100 도달하기/reach-100.py
+++ 251127/100 도달하기/reach-100.py
@@ -1,9 +1,14 @@
+# 입력받은 정수를 배열의 두번째 인덱스에 저장
 n = int(input())
-arr = [1,n]
+arr = [1, n]
+cnt = 1
 
-for i in range(1,10):
-    if arr[i] >= 100 :
-        break
-    else:
-        arr.append(arr[-1]+arr[-2])
+# 배열을 전전항과 전항을 더한 수로 채워나가고, 그 값이 100을 초과하면 while문을 탈출
+while True:
+	cnt += 1
+	arr.append(arr[cnt - 1] + arr[cnt - 2])
+	if arr[cnt] > 100:
+		break
+
+# 100을 딱 초과한 항까지 배열의 인덱스에 저장된 값을 출력
 print(*arr)
