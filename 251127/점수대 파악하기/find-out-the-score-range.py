040caeece663a7d6194a469cc686a1e6b2ba7285
JungWoon <64579991+wjddns0122@users.noreply.github.com>
2025-11-27 22:55:30 +0900

    Title: 점수대 파악하기, Time: 49ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251127/점수대 파악하기/find-out-the-score-range.py
+++ 251127/점수대 파악하기/find-out-the-score-range.py
@@ -1,12 +1,13 @@
-arr = list(map(int, input().split()))
-count_arr = [0] * 101
+arr = input().split()
+numbers = []
 
-for elem in arr:
-    elem = (elem // 10) * 10
-    count_arr[elem] += 1
-    if elem == 0:
-        break
+for i in arr:
+    if int(i) >= 10:
+        numbers.append(i)
 
-for i in range(100, 0, -10):
-    cnt = count_arr[i]
-    print(f"{i} - {cnt}")
+for i in range(100,9,-10):
+    cnt = 0
+    for j in numbers:
+        if (i/10) == (int(j)//10):
+            cnt += 1
+    print(f'{i} - {cnt}')
