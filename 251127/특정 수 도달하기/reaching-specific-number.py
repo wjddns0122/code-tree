d94481fe62bb89e9a6b048e7bdc6c68029b0b8ba
JungWoon <64579991+wjddns0122@users.noreply.github.com>
2025-11-27 19:53:36 +0900

    Title: 특정 수 도달하기, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 251127/특정 수 도달하기/reaching-specific-number.py
+++ 251127/특정 수 도달하기/reaching-specific-number.py
@@ -1,15 +1,12 @@
 arr = list(map(int, input().split()))  
 result = []
 
-# 반복문에서 마지막 숫자를 제외한 값 처리
-for num in arr[:-1]:
+for num in arr:
     if num >= 250:
         break
     result.append(num)
 
-
-# 총합과 평균 계산
-total = sum(result)  # 모든 값 더하기
+total = sum(result)  
 average = total / len(result) if len(result) > 0 else 0  # 평균 구하기 (0으로 나누는 오류 방지)
 
 print(f"{total} {average:.1f}")
