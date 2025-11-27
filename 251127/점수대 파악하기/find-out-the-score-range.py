c3063df9c49e4cecd911852437fd8afa83114e6c
JungWoon <64579991+wjddns0122@users.noreply.github.com>
2025-11-27 22:55:56 +0900

    Title: 점수대 파악하기, Time: 41ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251127/점수대 파악하기/find-out-the-score-range.py
+++ 251127/점수대 파악하기/find-out-the-score-range.py
@@ -8,6 +8,6 @@ for i in arr:
 for i in range(100,9,-10):
     cnt = 0
     for j in numbers:
-        if (i/10) == (int(j)//10):
+        if (i//10) == (int(j)//10):
             cnt += 1
     print(f'{i} - {cnt}')
