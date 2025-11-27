3f7e84cfb9a31355e074540659e2f4c7fa968782
JungWoon <64579991+wjddns0122@users.noreply.github.com>
2025-11-27 22:28:27 +0900

    Title: 1-9 개수 세기, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 251127/1-9 개수 세기/count-one-to-nine.py
+++ 251127/1-9 개수 세기/count-one-to-nine.py
@@ -1,15 +1,14 @@
-# 입력받은 정수를 배열의 두번째 인덱스에 저장
+# 원소 수 n 입력받기
 n = int(input())
-arr = [1, n]
-cnt = 1
 
-# 배열을 전전항과 전항을 더한 수로 채워나가고, 그 값이 100을 초과하면 while문을 탈출
-while True:
-	cnt += 1
-	arr.append(arr[cnt - 1] + arr[cnt - 2])
-	if arr[cnt] > 100:
-		break
+# 배열에 주어진 수를 입력받아 저장
+arr = list(map(int, input().split()))
+count_arr = [0] * 10
 
-# 100을 딱 초과한 항까지 배열의 인덱스에 저장된 값을 출력
+# 카운팅 배열을 통해 각각의 빈도 저장
 for elem in arr:
-	print(elem, end=" ")
+	count_arr[elem] += 1
+	
+# 1부터 9까지 나온 횟수를 출력
+for i in range(1, 10):
+	print(count_arr[i])
