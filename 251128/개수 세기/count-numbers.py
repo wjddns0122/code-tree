cf3f47c02d9558e781becb1dd317ac8e9f0a0fb3
JungWoon <64579991+wjddns0122@users.noreply.github.com>
2025-11-28 10:47:11 +0900

    Title: 개수 세기, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 251128/개수 세기/count-numbers.py
+++ 251128/개수 세기/count-numbers.py
@@ -1,9 +1,7 @@
-n, m = list(map(int, input().split()))
+n,m = map(int, input().split())
 arr = list(map(int, input().split()))
 cnt = 0
-
-for num in arr:
-    if num == 3:
-        cnt += 1
-    
+for i in range(n):
+    if arr[i] == m:
+        cnt += 1 
 print(cnt)
