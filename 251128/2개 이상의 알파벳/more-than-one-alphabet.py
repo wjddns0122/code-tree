c6c1bd50e37eba45d8369a253a405f40d56f0ee3
JungWoon <64579991+wjddns0122@users.noreply.github.com>
2025-11-28 22:22:40 +0900

    Title: 2개 이상의 알파벳, Time: 47ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251128/2개 이상의 알파벳/more-than-one-alphabet.py
+++ 251128/2개 이상의 알파벳/more-than-one-alphabet.py
@@ -1,17 +1,13 @@
 A = input()
-# Please write your code here.
-def sd(A):
-    cnt = 0
-    for i in range(len(A)):
-        if A[i] == A[i + 1]:
-            cnt += 1
-            if cnt >= 2:
-                break
-            return False
-        else:
-            return True
 
-if sd(A) == True:
+def sd(s):
+    # 인접한 두 글자만 비교하면 되니까 len(s) - 1 까지만
+    for i in range(len(s) - 1):
+        if s[i] == s[i + 1]:
+            return False   # 나쁜 경우 발견 → False
+    return True            # 끝까지 문제 없으면 True
+
+if sd(A):
     print("Yes")
 else:
     print("No")
