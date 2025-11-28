54e6b266edcaacdfa365ae3e87372d37c6117e0c
JungWoon <64579991+wjddns0122@users.noreply.github.com>
2025-11-28 18:13:24 +0900

    Title: 2021년 날짜의 유무, Time: 49ms, Memory: 16MB, Status: Passed - Codetree

--- 251128/2021년 날짜의 유무/with-or-without-2021.py
+++ 251128/2021년 날짜의 유무/with-or-without-2021.py
@@ -1,21 +1,24 @@
 M, D = map(int, input().split())
 
-# Please write your code here.
-def is_thiryone(M, D):
-    if M == 1 or M == 3 or M == 5 or M == 7 or M == 8 or M == 10 or M == 12:
-        if D <= 31:
-            print("Yes")
-        else:
-            print("No")
-    elif M == 4 or M == 6 or M == 9 or M == 11:
-        if D <= 30:
-            print("Yes")
-        else:
-            print("No")
-    else:
-        if D <= 28:
-            print("Yes")
-        else:
-            print("No")
+def is_valid_date(M, D):
+    # 1. 월 범위 체크
+    if not (1 <= M <= 12):
+        return False
 
-is_thiryone(M, D)
+    # 2. 각 달의 마지막 날 정의
+    days_in_month = [0,  # 0번 인덱스 더미
+                     31, 28, 31, 30, 31, 30,
+                     31, 31, 30, 31, 30, 31]
+
+    last = days_in_month[M]
+
+    # 3. 일 범위 체크
+    if 1 <= D <= last:
+        return True
+    else:
+        return False
+
+if is_valid_date(M, D):
+    print("Yes")
+else:
+    print("No")
