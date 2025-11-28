7fe1e51999974c3b3f50f9142d9b07a2809290d0
JungWoon <64579991+wjddns0122@users.noreply.github.com>
2025-11-28 14:39:45 +0900

    Title: 함수를 이용한 369 게임, Time: 85ms, Memory: 18MB, Status: Passed - Codetree

--- 251128/함수를 이용한 369 게임/369-games-using-functions.py
+++ 251128/함수를 이용한 369 게임/369-games-using-functions.py
@@ -1,8 +1,22 @@
 a, b = map(int, input().split())
 cnt = 0
-# Please write your code here.
+
+def is_369(n):
+    # 1) 3의 배수이면 바로 True
+    if n % 3 == 0:
+        return True
+    
+    # 2) 각 자리 수에 3, 6, 9가 있는지 확인
+    while n > 0:
+        digit = n % 10       # 가장 오른쪽 자리
+        if digit in (3, 6, 9):
+            return True
+        n //= 10             # 한 자리 줄이기
+    
+    return False             # 위에 안 걸리면 False
+
 for i in range(a, b + 1):
-    if i % 3 == 0 or i // 10 == 3 or i // 10 == 6 or i // 10 == 9 or i % 10 == 3 or i % 10 == 6 or i % 10 == 9:
+    if is_369(i):
         cnt += 1
 
 print(cnt)
