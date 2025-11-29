1c9539c4c7594236cee53f99952eda08c137ec1c
JungWoon <64579991+wjddns0122@users.noreply.github.com>
2025-11-29 22:34:26 +0900

    Title: 중앙값 계산 2, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 251129/중앙값 계산 2/get-median-2.py
+++ 251129/중앙값 계산 2/get-median-2.py
@@ -1,8 +1,11 @@
+# 변수 선언 및 입력:
 n = int(input())
-arr = list(map(int, input().split()))#입력 받은 수를 저장하는 배열 
-temp = []
-for idx, num in enumerate(arr, start=1):
-    temp.append(num)
-    if idx%2!=0:#인덱스가 홀수 일때, 해당 인덱스 까지 입력 받은 수들을 정렬하여 중앙값을 출럭할 것
-        temp.sort()
-        print(temp[len(temp)//2], end=" ")
+arr = list(map(int, input().split()))
+
+# 홀수 번째 수를 지날때마다 정렬을 진행한 후 가운데 값을 출력합니다.
+for i in range(n):
+    if i % 2 == 0:
+        # 오름차순 정렬을 진행합니다.
+        sorted_arr = sorted(arr[:i + 1])
+        # 가운데 값을 출력합니다.
+        print(sorted_arr[i // 2], end=" ")
