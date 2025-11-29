bb4ca89b6c7b7b008432939c27354d4ddcc08dc6
JungWoon <64579991+wjddns0122@users.noreply.github.com>
2025-11-29 19:22:58 +0900

    Title: 홀수 짝수에 따른 출력값, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 251129/홀수 짝수에 따른 출력값/output-value-based-on-odd-even-numbers.py
+++ 251129/홀수 짝수에 따른 출력값/output-value-based-on-odd-even-numbers.py
@@ -1,18 +1,15 @@
 n = int(input())
 
-def sum_even(x):
-    # 2 + 4 + ... + x  (x는 짝수)
-    if x == 0:
-        return 0
-    return x + sum_even(x - 2)
 
-def sum_odd(x):
-    # 1 + 3 + ... + x  (x는 홀수)
-    if x == 1:
+# 1부터 n까지의 n과 홀짝이 같은 수들의 합을 반환합니다.
+def get_num(n):
+    if n == 1:
         return 1
-    return x + sum_odd(x - 2)
+    if n == 2:
+        return 2
 
-if n % 2 == 0:          # n이 짝수면 짝수들만 더함
-    print(sum_even(n))
-else:                   # n이 홀수면 홀수들만 더함
-    print(sum_odd(n))
+    # n과 홀짝이 같은 수만을 재귀함수로 호출합니다.
+    return get_num(n - 2) + n
+
+
+print(get_num(n))
