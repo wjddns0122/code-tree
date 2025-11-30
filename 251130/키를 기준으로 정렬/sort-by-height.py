20283f7fa2b036d12a11d8f1b35f3ae1c29388d6
JungWoon <64579991+wjddns0122@users.noreply.github.com>
2025-11-30 16:17:01 +0900

    Title: 키를 기준으로 정렬, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 251130/키를 기준으로 정렬/sort-by-height.py
+++ 251130/키를 기준으로 정렬/sort-by-height.py
@@ -1,19 +1,13 @@
-# 클래스 선언
-class Student:
-    def __init__(self, name, height, weight):
-        self.name, self.height, self.weight = name, height, weight
-
-
 # 변수 선언 및 입력
 n = int(input())
 students = []
 for _ in range(n):
     name, height, weight = tuple(input().split())
-    students.append(Student(name, int(height), int(weight)))
+    students.append((name, int(height), int(weight)))
 
 # Custom Comparator를 활용한 정렬
-students.sort(key = lambda x: x.height)
+students.sort(key = lambda x: x[1])
 
 # 출력
-for student in students:
-    print(student.name, student.height, student.weight)
+for name, height, weight in students:
+    print(name, height, weight)
