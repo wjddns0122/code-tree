e7debecfa868483ffd235a07e13dad47dabe0d17
JungWoon <64579991+wjddns0122@users.noreply.github.com>
2025-11-30 19:47:12 +0900

    Title: DateTime to DateTime, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 251130/DateTime to DateTime/datetime-to-datetime.py
+++ 251130/DateTime to DateTime/datetime-to-datetime.py
@@ -1,21 +1,14 @@
 a, b, c = map(int, input().split())
-ellapsed_min = 0
-# Please write your code here.
-while True:
-    if a == 11 and b == 11 and c == 11:
-        break
-    
-    if a < 11:
-        print(-1)
 
-    ellapsed_min += 1
-    c -= 1
-    if c == 0:
-        b -= 1
-        c = 60
-
-    if b == 0:
-        a -= 1
-        b = 24
-
-print(ellapsed_min)
+# 기준 시각보다 이전인 경우
+if a < 11:
+    print(-1)
+elif a == 11 and b < 11:
+    print(-1)
+elif a == 11 and b == 11 and c < 11:
+    print(-1)
+else:
+    # 기준: 11일 11시 11분
+    # 경과 분 = (날짜 차이 * 24 + 시 차이) * 60 + 분 차이
+    elapsed_min = ((a - 11) * 24 + (b - 11)) * 60 + (c - 11)
+    print(elapsed_min)
